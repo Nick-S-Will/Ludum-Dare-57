3b6c845b630fa86d3e25d12b411ba33e35bc278c
Nicholas Williams <nickwill820@gmail.com>
2025-04-05 18:20:29 -0400

    Made money UI update smoothly

--- Assets/Scripts/Shopping/UI/MoneyUI.cs
+++ Assets/Scripts/Shopping/UI/MoneyUI.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Assertions;
@@ -8,8 +9,12 @@ namespace LudumDare57.Shopping.UI
     {
         [SerializeField] private MoneyHandler moneyHandler;
         [SerializeField] private TMP_Text moneyText;
+        [Header("Attributes")]
+        [SerializeField][Min(0f)] private float changeMoneyTime = 1f;
 
+        private Coroutine changeMoneyRoutine;
         private string textFormat;
+        private int displayMoney;
 
         private void Awake()
         {
@@ -18,12 +23,37 @@ namespace LudumDare57.Shopping.UI
             moneyHandler.MoneyChanged.AddListener(UpdateUI);
             textFormat = moneyText.text;
 
-            UpdateUI();
+            UpdateMoneyText();
         }
 
         private void UpdateUI()
         {
-            moneyText.text = string.Format(textFormat, moneyHandler.Money);
+            if (changeMoneyRoutine != null) StopCoroutine(changeMoneyRoutine);
+
+            changeMoneyRoutine = StartCoroutine(ChangeMoney(moneyHandler.Money));
         }
+
+        private IEnumerator ChangeMoney(int targetMoney)
+        {
+            if (displayMoney == targetMoney)
+            {
+                changeMoneyRoutine = null;
+                yield break;
+            }
+
+            float changeInterval = changeMoneyTime / Mathf.Abs(targetMoney - displayMoney);
+            int changeDirection = (int)Mathf.Sign(targetMoney - displayMoney);
+            while (displayMoney != targetMoney)
+            {
+                displayMoney += changeDirection;
+                moneyText.text = string.Format(textFormat, displayMoney);
+
+                yield return new WaitForSeconds(changeInterval);
+            }
+
+            changeMoneyRoutine = null;
+        }
+
+        private void UpdateMoneyText() => moneyText.text = string.Format(textFormat, displayMoney);
     }
 }
