8ac2cd23c6ed2417cd3aab147d2356a56b71c16b
Nicholas Williams <nickwill820@gmail.com>
2025-04-06 11:50:51 -0400

    Created fish assets and modified import settings

--- Assets/Scripts/Fishing/Fish.cs
+++ Assets/Scripts/Fishing/Fish.cs
@@ -9,11 +9,12 @@ namespace LudumDare57.Fishing
         public int Price => price;
         public float CatchTime => catchTime;
         public Sprite Sprite => sprite;
+        public Sprite SilhouetteSprite => silhouetteSprite;
 
         [SerializeField] private string description = "Yohohoho.";
         [SerializeField][Min(1f)] private int price = 10;
-        [SerializeField][Min(1e-5f)] private float catchTime = .5f;
-        [SerializeField] private Sprite sprite;
+        [SerializeField][Min(1e-5f)] private float catchTime = 1f;
+        [SerializeField] private Sprite sprite, silhouetteSprite;
 
         public override string ToString()
         {
