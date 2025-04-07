d49af42297ae3af779219fb8d5a9a1e6cd95b16e
Nicholas Williams <nickwill820@gmail.com>
2025-04-06 23:49:00 -0400

    Added credits to title scene

--- Assets/Scripts/Title/TitleSceneFunctions.cs
+++ Assets/Scripts/Title/TitleSceneFunctions.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
 
 namespace LudumDare57.Title
 {
     public class TitleSceneFunctions : MonoBehaviour
     {
-        [SerializeField] private MonoBehaviour credits;
+        [SerializeField] private GameObject creditsObject;
         [SerializeField] private string gameSceneName = "GameScene";
         [SerializeField] private string quitRedirectURL = "https://youtu.be/14FOPsSCIPs?si=qXa2XigqFUfiPRJ9";
+
+        private void Awake()
+        {
+            Assert.IsNotNull(creditsObject);
+        }
 
         public void StartGame()
         {
@@ -16,7 +22,7 @@ namespace LudumDare57.Title
 
         public void ToggleCredits()
         {
-            credits.enabled = !credits.enabled;
+            creditsObject.SetActive(!creditsObject.activeSelf);
         }
 
         public void QuitGame()
